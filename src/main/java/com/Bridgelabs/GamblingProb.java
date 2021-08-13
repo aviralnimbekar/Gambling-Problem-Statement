d38c4f6f98e9b42c9837cc504d37b27f788da230
amitnimbekar <amit.nimbekar@actifio.com>
2021-08-13 08:45:34 +0530

    ADDING : Initial Variables with correction

--- src/main/java/com/Bridgelabs/GamblingProb.java
+++ src/main/java/com/Bridgelabs/GamblingProb.java
@@ -2,8 +2,8 @@ package com.Bridgelabs;
 
 public class GamblingProb {
 
-    static int INITIALSTAKE = 100;
-    static int BETSTAKE = 1;
+    int INITIALSTAKE = 100;
+    int BETSTAKE = 1;
 
     public static void main(String[] args) {
         System.out.println("Welcome to Gambling Problem");
