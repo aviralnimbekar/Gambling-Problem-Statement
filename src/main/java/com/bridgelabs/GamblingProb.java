499f466305352bda5a3d89aa6bc8be5419eb5fa0
amitnimbekar <amit.nimbekar@actifio.com>
2021-08-13 09:22:47 +0530

    ADDING : Betting condition

--- src/main/java/com/bridgelabs/GamblingProb.java
+++ src/main/java/com/bridgelabs/GamblingProb.java
@@ -2,8 +2,27 @@ package com.bridgelabs;
 
 public class GamblingProb {
 
-    int INITIALSTAKE = 100;
-    int BETSTAKE = 1;
+    public static final int INITIALSTAKE = 100;
+    public static final int BETSTAKE = 1;
+
+    /**
+     * making bet
+     *
+     * @return totalStake
+     */
+    public int makeBet() {
+
+        int win = 1;
+        int totalStake = 100;
+        int random = (int) (Math.random() * 2);
+
+        if (random == win) {
+            totalStake++;
+        } else {
+            totalStake--;
+        }
+        return totalStake;
+    }
 
     public static void main(String[] args) {
         System.out.println("Welcome to Gambling Problem");
