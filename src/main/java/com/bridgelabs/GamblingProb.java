dcdf72bf7959918a752c8f0bd64cb394ac28b879
amitnimbekar <amit.nimbekar@actifio.com>
2021-08-14 01:00:18 +0530

    ADDING : Resignation condition

--- src/main/java/com/bridgelabs/GamblingProb.java
+++ src/main/java/com/bridgelabs/GamblingProb.java
@@ -2,30 +2,64 @@ package com.bridgelabs;
 
 public class GamblingProb {
 
-    public static final int INITIALSTAKE = 100;
-    public static final int BETSTAKE = 1;
+    public static final int INITIAL_STAKE = 100;
+    public static final int BET_STAKE = 1;
+    public static int finalSakeForDay, initialStakeForDay;
+
 
     /**
-     * making bet
+     * making bet for win or lose
      *
-     * @return totalStake
+     * @return finalSakeForDay
+     * variable: initialStakeForDay
      */
-    public void makeBet() {
+    public static int makeBet() {
 
-        int win = 1;
+        int WIN = 1;
         int random = (int) (Math.random() * 2);
 
-        if (random == win) {
-            System.out.println("WON");;
+        if (random == WIN) {
+            System.out.println("***WON***");
+            initialStakeForDay++;
         } else {
-            System.out.println("LOSE");
+            System.out.println("***LOSE***");
+            initialStakeForDay--;
         }
+        return initialStakeForDay;
     }
 
+    /**
+     * Betting for single day only
+     *
+     * @return finalStakeForDay
+     */
+    public static int resignationForDay() {
+
+        int lowerAmount = INITIAL_STAKE - INITIAL_STAKE / 2;
+        int upperAmount = INITIAL_STAKE + INITIAL_STAKE / 2;
+        initialStakeForDay = 100;
+        boolean run = true;
+
+        while (run) {
+
+            makeBet();
+            if (initialStakeForDay <= lowerAmount)
+                run = false;
+            if (initialStakeForDay >= upperAmount)
+                run = false;
+        }
+        finalSakeForDay = initialStakeForDay;
+        return finalSakeForDay;
+    }
+
+    /**
+     * Purpose - Execution of program by calling required method
+     * @param args resignationForDay
+     */
     public static void main(String[] args) {
         System.out.println("Welcome to Gambling Problem");
 
-        GamblingProb game = new GamblingProb();
-        game.makeBet();
+        System.out.println("Gambler reached to " + resignationForDay() + " and stopped for day");
     }
 }
+
